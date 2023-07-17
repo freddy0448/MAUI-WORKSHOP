a8752c07d1e0a50caf3747bd18a212821dadbe7c
freddy048 <20211800@itla.edu.do>
2023-07-17 15:54:08 -0400

    Creando el ViewModel para poder bindear hacia la vista. Aqui uso la interfaz INotifyPropertyChanged

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Microsoft.Maui.Controls.Compatibility;
+using System.ComponentModel;
 
 namespace MAUI_WORKSHOP.ViewModel
 {
-    internal class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public string Text 
+        {
+            get => Text;
+            set
+            {
+                Text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged; //Implemento la interfaz
+        
+        void OnPropertyChanged(string propertyName) //Creo un metodo que usa
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); // Esto levanta el evento PropertieChanged y cambiara la propiedad indicada
+        }
     }
 }
